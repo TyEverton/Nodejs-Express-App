9eaa52dbd3b246f9c4cb18a0b8d7b83e29811cea
Ty Everton <everton_915@yahoo.com>
2021-08-25 12:02:25 -0600

    section 7 video 3

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express') //calls the express library and gives us access to the library
 const app = express() //instance of express
+const chalk = require('chalk')
+const debug = require('debug')('server')
+const morgan = require('morgan')
+const path = require('path')
+const PORT = process.env.PORT || 3000
+const sessionsRouter = require('./src/router/sessionsRouter')
+const adminRouter = require('./src/router/adminRouter')
+
+app.use(morgan('tiny'))
+app.use(express.static(path.join(__dirname, '/public/')))
+
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
+
+app.use('/sessions', sessionsRouter)
+app.use('/admin', adminRouter)
 
 app.get('/', (req, res) => {
-   res.send('Hello from my app!')
+  res.render('index', { title: 'Globomantics', data: ['a', 'b', 'c'] })
 })
 
-app.listen(6000, ()=> {
-  console.log(`Server bumpin' on port 6000`)
+app.listen(PORT, () => {
+  debug(`to port ${chalk.blue(PORT)}`)
 })
